05833351e4d60f8e7ec562ed3e028278e4d1efc2
litianao <exrobbie@gmail.com>
2018-09-23 16:30:09 +0800

    added footer links

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,98 +1,61 @@
 <template>
   <footer class="footer">
-    <!--<nav class="social-links">-->
-      <!--<ul>-->
-        <!--<li>-->
-          <!--<a href="//twitter.com/DappPub" target="_blank">-->
-            <!--<img -->
-              <!--class="footer-icon"-->
-              <!--:src="iconTwitter" -->
-              <!--alt="twitter" />-->
-          <!--</a>-->
-        <!--</li>-->
-        <!--<li>-->
-          <!--<a href="//github.com/dappub" target="_blank">-->
-            <!--<img -->
-              <!--class="footer-icon"-->
-              <!--:src="iconGithub" -->
-              <!--alt="github" />-->
-          <!--</a>-->
-        <!--</li>-->
-        <!--<li>-->
-          <!--<a href="//medium.com/dapppub" target="_blank">-->
-            <!--<img -->
-              <!--class="footer-icon"-->
-              <!--:src="iconMedium" -->
-              <!--alt="medium" />-->
-          <!--</a>-->
-        <!--</li>-->
-        <!--<li>-->
-          <!--<a href="//www.reddit.com/user/dapppub" target="_blank">-->
-            <!--<img -->
-              <!--class="footer-icon"-->
-              <!--:src="iconReddit" -->
-              <!--alt="reddit" />-->
-          <!--</a>-->
-        <!--</li>-->
-      <!--</ul>-->
-    <!--</nav>-->
-    <p class="copyright">Copyright &copy; 2018. Dexlize Foundation PTE. LTD.</p>
+    <div class="footer-item">Copyright &copy; 2018. Dexlize Foundation PTE. LTD.</div>
+    <div class="footer-item">
+      <div class="footer-item-title">
+        相关协议
+      </div>
+      <div class="footer-item-list">
+        <a target="_blank" href="https://dapp.mytokenpocket.vip/%E5%9F%BA%E4%BA%8E%E7%8F%AD%E6%9F%AF%E7%9A%84%E5%AE%9E%E7%94%A8%E5%A2%9E%E5%BC%BA%E5%9E%8B%E9%80%9A%E8%AF%81%E5%8D%8F%E8%AE%AE.pdf">基于班柯的实用增强型通证协议</a>
+      </div>
+    </div>
+    <div class="footer-item">
+      <div class="footer-item-title">网站</div>
+      <div class="footer-item-list">
+        <a target="_blank" href="https://dapp.pub/">DappPub</a>
+      </div>
+      <div class="footer-item-list">
+        <a target="_blank" href="http://www.dexlize.org/">Dexlize</a>
+      </div>
+    </div>
+
   </footer>
 </template>
 
 <script>
-  // export default {
-  //   data() {
-  //     return {
-  //       iconGithub: require('@/assets/github.svg'),
-  //       iconTwitter: require('@/assets/twitter.svg'),
-  //       iconMedium: require('@/assets/medium.svg'),
-  //       iconReddit: require('@/assets/reddit.svg')
-  //     };
-  //   }
-  // };
+
 </script>
 
 <style scoped>
   .footer {
-    padding: 15px 100px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    background-color: #fff;
-    text-align: center;
-  } 
-
-  /*.footer-icon {*/
-    /*width: 30px;*/
-  /*}*/
-
-  /*.social-links > ul {*/
-    /*display: flex;*/
-    /*align-items: center;*/
-    /*justify-content: center;*/
-  /*}*/
-
-  /*.social-links > ul > li:not(:last-child) {*/
-    /*margin-right: 60px;*/
-  /*}*/
-
-  /*.social-links > ul > li {*/
-    /*cursor: pointer;*/
-    /*transition: margin 200ms ease;*/
-    /*opacity: .5;*/
-  /*}*/
-
-  /*.social-links > ul > li:hover {*/
-    /*margin-top: -10px;*/
-    /*opacity: 1;*/
-  /*}*/
-
-  .copyright {
     color: #888;
     font-size: .8em;
     font-weight: 300;
     line-height: 24px;
+    padding: 15px 32px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-wrap: wrap;
+    background-color: #fff;
+    text-align: center;
+  }
+
+  .footer-item{
+    flex: 1;
+    flex-basis: 100%;
+    margin-bottom: 16px;
+  }
+
+  .footer-item-title{
+    color: #3d5170;
+    font-size: 16px;
+    margin-bottom: 8px;
+  }
+
+  .footer-item a{
+    color: #888;
+    text-decoration: none;
   }
 </style>
 
