faca297bbaf08e3760b6b7e5268b4361ee8fc87b
litianao <exrobbie@gmail.com>
2018-09-23 09:36:16 +0800

    add flex to tbody

--- src/pages/components/orders.vue
+++ src/pages/components/orders.vue
@@ -208,6 +208,11 @@ export default {
     padding-bottom: 12px;
 }
 
+.my-orders-table tbody{
+    display: flex;
+    flex-wrap: wrap;
+}
+
 .my-orders-table tbody td{
     flex: 1;
     border-bottom: 1px solid rgba(0,0,0,.1);
