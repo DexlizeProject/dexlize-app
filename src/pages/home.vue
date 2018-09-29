afe30d4dc4d4d3f425f1e4c4867f391be618601d
apathymagic <apathymagic@163.com>
2018-09-29 19:12:04 +0800

    home list and price and change

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -6,61 +6,98 @@
 </template>
 
 <script>
-    export default {
-        data(){
-            return{
-                tradingData: []
+import api from '@/utils/eos';
+import fetch from '@/utils/api';
+import {hexTransform, assetTransform} from '@/utils/math';
+
+export default {
+    mounted() {
+        this.getData();
+    },
+    data(){
+        return{
+            tradingData: []
+        }
+    },
+
+    methods: {
+        getData() {
+            let product = 'Dapppub';
+            switch (product) {
+                case 'Dapppub':
+                this.getDapppubTradingData();
+                break;
+                case 'Kyubey':
+                this.getKyubeyTradingData();
+                break;
+                default:
             }
         },
-        created(){
-            this.tradingData = [
-                {
-                    tradingPair: 'PUB/EOS',
-                    price: '0.03313123',
-                    change: '+99.99%'
-                },
-                {
-                    tradingPair: 'PUB/EOS',
-                    price: '0.03131123',
-                    change: '+99.99%'
-                },
-                {
-                    tradingPair: 'PUB/EOS',
-                    price: '0.03131123',
-                    change: '+99.99%'
-                },
-                {
-                    tradingPair: 'PUB/EOS',
-                    price: '0.03131123',
-                    change: '+99.99%'
-                },
-                {
-                    tradingPair: 'PUB/EOS',
-                    price: '0.03131312',
-                    change: '+99.99%'
-                },
-                {
-                    tradingPair: 'PUB/EOS',
-                    price: '0.03131312',
-                    change: '+99.99%'
-                },
-                {
-                    tradingPair: 'PUB/EOS',
-                    price: '0.03131312',
-                    change: '+99.99%'
-                },
-                {
-                    tradingPair: 'PUB/EOS',
-                    price: '0.03131312',
-                    change: '+99.99%'
-                }
-            ]
+        getDapppubTradingData() {
+            let tokens = ['PUB', 'TPT'];
+            tokens.forEach(token => {
+                api.getTableRows({
+                    json: true,
+                    code: 'tokendapppub',
+                    scope: token,
+                    table: 'games'
+                }).then(({ rows }) => {
+                    let currentPrice = (1 / ((parseInt(rows[0].stake) / 10000) / (hexTransform(rows[0].eos)))).toFixed(8);
+                    // get change of current token
+                    fetch(`token/kline?symbol=${token}&interval=1d&limit=2`).then(({ data }) => {
+                        let change = ((data[1].high - data[0].high) / data[0].high * 100).toFixed(4);
+                        change = change > 0 ?  '+' + change + '%' : change + '%';
+                        this.tradingData.push({
+                            tradingPair: token + '/EOS',
+                            price: currentPrice,
+                            change: change
+                        });
+                    });
+                });
+            });
         },
-        components: {
-            tradingSwitch: require('@/pages/components/trading-switch').default,
-            trading: require('@/pages/components/trading').default
-        }
+        getKyubeyTradingData() {
+            let tokens = ['KBY'];
+            tokens.forEach(token => {
+                api.getTableRows({
+                    json: true,
+                    code: 'myeosgroupon',
+                    scope: 'myeosgroupon',
+                    table: 'global'
+                }).then(({rows}) => {
+                    let global = rows[0];
+
+                    api.getTableRows({
+                        json: true,
+                        code: 'dacincubator',
+                        scope: 'dacincubator',
+                        table: 'market'
+                    }).then(({rows}) => {
+                        let market = rows[0];
+
+                        // calculate the current price of KBY
+                        let buy_eos_quantity = 100;
+                        let eos_amout = assetTransform(market.balance) + assetTransform(global.reserve);
+                        let K = 10000000000;
+                        let kby_quantity = (Math.sqrt(eos_amout * 2 * K) * 100 - assetTransform(market.supply)) * buy_eos_quantity / assetTransform(global.reserve);
+                        let currentPrice = (100 / kby_quantity).toFixed(8);
+
+                        // push the current trading data of KBY
+                        this.tradingData.push({
+                            tradingPair: token + '/EOS',
+                            price: currentPrice,
+                            change: '+99.00%'
+                        });
+                    });
+                });
+            });
+        },
+    },
+    components: {
+        tradingSwitch: require('@/pages/components/trading-switch').default,
+        trading: require('@/pages/components/trading').default
     }
+}
 </script>
 
 <style>
--- src/utils/math.js
+++ src/utils/math.js
@@ -1,13 +1,13 @@
 export const feePercent = raw => {
-  const { 
-    init_fee_percent, 
+  const {
+    init_fee_percent,
     base_fee_percent,
     start_time,
     lock_up_period
-  } = raw; 
+  } = raw;
   const now = (Date.now() / 1000);
   if ((init_fee_percent === base_fee_percent) || (now >= start_time + lock_up_period)) return base_fee_percent;
-  
+
   return Math.ceil(parseFloat(2 * lock_up_period * (init_fee_percent- base_fee_percent) / ((now - start_time) + lock_up_period) + 2 * base_fee_percent - init_fee_percent)).toFixed(0);
 };
 
@@ -15,3 +15,6 @@ export const hexTransform = raw => {
   return Number('0x' + raw.slice(2).match(/\w{2}/g).reverse().join('')) / 10000;
 };
 
+export const assetTransform = raw => {
+    return parseInt(raw.substring(0, raw.length - 4) * 10000 + 0.5);
+};
