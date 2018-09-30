82a0a9dc94e269214a7da93ca90fdc82dd63f09f
litianao <exrobbie@gmail.com>
2018-09-30 17:53:30 +0800

    added webpack bundle analyzer

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,15 @@
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
 module.exports = {
   baseUrl: process.env.NODE_ENV === 'production' ? '/' : '/',
   devServer: {
     proxy: ''
-  }
+  },
+    configureWebpack:{
+    plugins:[
+        new BundleAnalyzerPlugin()
+    ]
+    }
   // protocol_types: [{
   // 	'protocol_name': 'PUB',
   // 	'description': {
